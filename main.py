5333346833ebce97abd851eb7e5512b14afeff35
lacksfish <lacksfish@gmail.com>
2021-05-04 20:18:27 +0200

    handle disconnects, wait on prev message before playing next one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import re
+import time
 import gtts
 import socket
 import tempfile
@@ -50,54 +51,68 @@ def main():
     connected = False
 
     while True:
-        resp = sock.recv(2048).decode('utf-8')
+        try:
+            resp = sock.recv(2048).decode('utf-8')
 
-        if resp and not connected:
-            print(f'Connected to {nickname}!')
-            connected = True
+            if resp and not connected:
+                print(f'Connected to {nickname}!')
+                connected = True
 
-        if resp.startswith('PING'):
-            sock.send("PONG\n".encode('utf-8'))
+            if resp.startswith('PING'):
+                sock.send("PONG\n".encode('utf-8'))
 
-        elif len(resp) > 0:
-                # If it is a user chat message
-                if "PRIVMSG" in resp:
-                    username, channel, message = re.search(':(.*)\!.*@.*\.tmi\.twitch\.tv PRIVMSG #(.*) :(.*)', resp).groups()
+            elif len(resp) > 0:
+                    # If it is a user chat message
+                    if "PRIVMSG" in resp:
+                        username, channel, message = re.search(':(.*)\!.*@.*\.tmi\.twitch\.tv PRIVMSG #(.*) :(.*)', resp).groups()
 
-                    if username.lower() == ignore_user.lower():
-                        log.info(f'Muted user not played: {ignore_user}')
-                        continue
-                    log.info(f'New message from {username}: {message}')
+                        if username.lower() == ignore_user.lower():
+                            log.info(f'Muted user not played: {ignore_user}')
+                            continue
+                        log.info(f'New message from {username}: {message}')
 
-                    # Delete old temp message mp3's
-                    mixer.music.unload()
-                    for f in os.listdir(tempdir):
+                        # Delete old temp message mp3's
+                        for f in os.listdir(tempdir):
+                            try:
+                                os.remove(os.path.join(tempdir, f))
+                            except:
+                                pass
+
+                        # Detect language of chat message
+                        text = TB(message)
+                        language = text.detect_language()
+
+                        # Create and save TTS mp3 file in temp folder
+                        # Try with detected language, otherwise just use english as a failsafe
                         try:
-                            os.remove(os.path.join(tempdir, f))
+                            tts = gtts.gTTS(f'{username}: {message}', lang=language)
                         except:
-                            pass
+                            tts = gtts.gTTS(f'{username}: {message}', lang='en')
+                        tempfile_name = next(tempfile._get_candidate_names())
+                        tts.save(tempdir + f'\{tempfile_name}.mp3')
 
-                    # Detect language of chat message
-                    text = TB(message)
-                    language = text.detect_language()
+                        # Play audio depending on operating system
+                        if os.name == 'nt':
+                            # Wait on previous message
+                            while mixer.music.get_busy():
+                                time.sleep(0.5)
+                            # Unload old messages
+                            mixer.music.unload()
+                            # Windows audio play
+                            mixer.music.load(tempdir + f'\{tempfile_name}.mp3')
+                            mixer.music.play()
+                        else:
+                            # Unix audio play
+                            playsound(tempdir + f'\{tempfile_name}.mp3')
+        except:
+            sock = socket.socket()
+            sock.connect((server, port))
+            sock.send(f"PASS {token}\n".encode('utf-8'))
+            sock.send(f"NICK {nickname}\n".encode('utf-8'))
+            sock.send(f"JOIN {channel}\n".encode('utf-8'))
 
-                    # Create and save TTS mp3 file in temp folder
-                    # Try with detected language, otherwise just use english as a failsafe
-                    try:
-                        tts = gtts.gTTS(f'{username}: {message}', lang=language)
-                    except:
-                        tts = gtts.gTTS(f'{username}: {message}', lang='en')
-                    tempfile_name = next(tempfile._get_candidate_names())
-                    tts.save(tempdir + f'\{tempfile_name}.mp3')
-
-                    # Play audio depending on operating system
-                    if os.name == 'nt':
-                        # Windows audio play
-                        mixer.music.load(tempdir + f'\{tempfile_name}.mp3')
-                        mixer.music.play()
-                    else:
-                        # Unix audio play
-                        playsound(tempdir + f'\{tempfile_name}.mp3')
+            print('Some darn error. Reconnecting to chat...')
+            connected = False
 
 
 if __name__ == '__main__':
